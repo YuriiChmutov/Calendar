0972c472346c7b56cf4307975c3bdd73ed900e8a
CallMeYurii <vad4807@gmail.com>
2021-11-21 20:17:21 +0200

    Push test branch

--- Calendar/Data/SampleData.cs
+++ Calendar/Data/SampleData.cs
@@ -1,10 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Calendar.Models;
 
 namespace Calendar.Data
 {
     public static class SampleData
     {
-        
+        public static void show()
+        {
+            Console.WriteLine("Hello");
+        }
     }
 }
